7992d365a169fc91f7eaa0515cd0c25d09508b79
JanickNorman <janicknorman@gmail.com>
2025-12-10 17:26:09 +0700

    loop assignments

--- src/main/java/com/example/footie/newSimulator/WorldCupDrawForwardChecking.java
+++ src/main/java/com/example/footie/newSimulator/WorldCupDrawForwardChecking.java
@@ -1,11 +1,13 @@
 package com.example.footie.newSimulator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
+import com.example.footie.newSimulator.constraint.AtMostTwoEuropeTeamsPerGroup;
 import com.example.footie.newSimulator.constraint.ConstraintManager;
-import com.example.footie.newSimulator.constraint.NoSameContinentInGroup;
+import com.example.footie.newSimulator.constraint.NoSameContinentInGroupForNonEurope;
 import com.example.footie.newSimulator.constraint.PairedGroupConstraint;
 
 public class WorldCupDrawForwardChecking {
@@ -16,7 +18,9 @@ public class WorldCupDrawForwardChecking {
         List<GroupSlot> slots = buildWorldCupSlots();
         // teams = teams.stream().filter(s -> s.getContinents().size() == 0).toList();
         ConstraintManager cm = new ConstraintManager();
-        cm.addConstraint(new NoSameContinentInGroup());
+        // cm.addConstraint(new NoSameContinentInGroupExceptEurope());
+        cm.addConstraint(new NoSameContinentInGroupForNonEurope());
+        cm.addConstraint(new AtMostTwoEuropeTeamsPerGroup());
         cm.addConstraint(new PairedGroupConstraint("Spain", Set.of("E", "I", "F", "H", "D", "G"), "Argentina",
                 Set.of("C", "A", "L", "J", "B", "K"), true));
 
@@ -27,32 +31,38 @@ public class WorldCupDrawForwardChecking {
         simulator.assignTeamToSlot("B1", "Canada");
         simulator.assignTeamToSlot("D1", "USA");
 
-        simulator.tryPlaceTeam("France", 1);
-        simulator.tryPlaceTeam("Germany", 1);
-        simulator.tryPlaceTeam("Brazil", 1);
-        simulator.tryPlaceTeam("Argentina", 1); // Group G
-        simulator.tryPlaceTeam("Spain", 1); // must go directly to J
-        simulator.tryPlaceTeam("Belgium", 1);
-        simulator.tryPlaceTeam("Croatia", 1);
-        simulator.tryPlaceTeam("England", 1);
-        simulator.tryPlaceTeam("Japan", 1);
-        simulator.tryPlaceTeam("Senegal", 3);
-        simulator.tryPlaceTeam("Portugal", 2);
-        simulator.tryPlaceTeam("Norway", 2);
-        simulator.tryPlaceTeam("Curacao", 3);
-        simulator.tryPlaceTeam("Ecuador", 4);
-        simulator.tryPlaceTeam("Switzerland", 3);
-        simulator.tryPlaceTeam("Tunisia", 4);
-        simulator.tryPlaceTeam("Uruguay", 4);
-        simulator.tryPlaceTeam("Ghana", 3);
-        simulator.tryPlaceTeam("SouthKorea", 3);
-        simulator.tryPlaceTeam("Australia");
-        simulator.tryPlaceTeam("CapeVerde");
+
+        // Collections.shuffle(teams);
+        teams.sort(Comparator.comparing(t -> t.pot()));
+        teams.forEach(t -> simulator.tryPlaceTeam(t.getName()));
+
+        // simulator.tryPlaceTeam("France", 1);
+        // simulator.tryPlaceTeam("Germany", 1);
+        // simulator.tryPlaceTeam("Brazil", 1);
+        // simulator.tryPlaceTeam("Argentina", 1); // Group G
+        // simulator.tryPlaceTeam("Spain", 1); // must go directly to J
+        // simulator.tryPlaceTeam("Belgium", 1);
+        // simulator.tryPlaceTeam("Croatia", 1);
+        // simulator.tryPlaceTeam("England", 1);
+        // simulator.tryPlaceTeam("Japan", 1);
+        // simulator.tryPlaceTeam("Senegal", 3);
+        // simulator.tryPlaceTeam("Portugal", 2);
+        // simulator.tryPlaceTeam("Norway", 2);
+        // simulator.tryPlaceTeam("Curacao", 3);
+        // simulator.tryPlaceTeam("Ecuador", 4);
+        // simulator.tryPlaceTeam("Switzerland", 3);
+        // simulator.tryPlaceTeam("Tunisia", 4);
+        // simulator.tryPlaceTeam("Uruguay", 4);
+        // simulator.tryPlaceTeam("Ghana", 3);
+        // simulator.tryPlaceTeam("SouthKorea", 3);
+        // simulator.tryPlaceTeam("Australia");
+        // simulator.tryPlaceTeam("CapeVerde");
+        // simulator.tryPlaceTeam("European Play-Off A");
         // // simulator.tryPlaceTeam("CotedIvoire", 2);
         // simulator.tryPlaceTeam("Haiti", 4);
 
         // System.out.println("Remaining groups with most slots: " + simulator.getNextGroupsToAssign());                
-        simulator.prettyPrintGroupAssignments();
+        simulator.prettyPrintGroupAssignmentsVertical();
     }
 
 
